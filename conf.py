d8e93dbf7d0701a0cf4f0648e23db27a5d33776e
saracuriel <144734028+saracuriel@users.noreply.github.com>
2025-11-26 11:52:57 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -79,7 +79,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo ='_images/erdera_logo (1).svg'
+html_logo ='_images/erdera_logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
